287db3cf4277c16170d79efb987d928aa9360e37
M4doesstuff <123054773+fdelavega02@users.noreply.github.com>
2026-08-10 12:35:04 -0400

    Revert virtual keyboard input experiment

--- desmume/src/frontend/posix/gtk/main.cpp
+++ desmume/src/frontend/posix/gtk/main.cpp
@@ -1994,34 +1994,12 @@ static void MenuSave(GSimpleAction *action, GVariant *parameter, gpointer user_d
     savegame(g_variant_get_uint32(parameter));
 }
 
-// Virtual keyboards frequently emit an uppercase keysym together with Shift,
-// while the default game bindings are lowercase. Normalize both forms so an
-// on-screen X/Z behaves exactly like a physical x/z key.
-static u16 LookupGameKey(guint keyval)
-{
-  u16 key = lookup_key(keyval);
-  if (!key) key = lookup_key(gdk_keyval_to_lower(keyval));
-  if (!key) key = lookup_key(gdk_keyval_to_upper(keyval));
-  return key;
-}
-
-static void LogGameKeyEvent(const char *type, GdkEventKey *e, u16 key)
-{
-  if (g_getenv("DESMUME_DEBUG_INPUT") == NULL)
-    return;
-
-  const gchar *name = gdk_keyval_name(e->keyval);
-  g_printerr("Project PM input: %s keyval=0x%x (%s) state=0x%x mapped=0x%x\\n",
-             type, e->keyval, name ? name : "unknown", e->state, key);
-}
-
 static gint Key_Press(GtkWidget *w, GdkEventKey *e, gpointer data)
 {
   guint mask;
-  mask = gtk_accelerator_get_default_mod_mask () & ~(GDK_SHIFT_MASK | GDK_LOCK_MASK);
+  mask = gtk_accelerator_get_default_mod_mask ();
   if( (e->state & mask) == 0){
-    u16 Key = LookupGameKey(e->keyval);
-    LogGameKeyEvent("press", e, Key);
+    u16 Key = lookup_key(e->keyval);
     if(Key){
       ADD_KEY( keys_latch, Key );
       return 1;
@@ -2039,8 +2017,7 @@ static gint Key_Press(GtkWidget *w, GdkEventKey *e, gpointer data)
 
 static gint Key_Release(GtkWidget *w, GdkEventKey *e, gpointer data)
 {
-  u16 Key = LookupGameKey(e->keyval);
-  LogGameKeyEvent("release", e, Key);
+  u16 Key = lookup_key(e->keyval);
   RM_KEY( keys_latch, Key );
   return 1;
 
